c2428680c605873bd0113bf95383664d3c7a8367
Sandeep Kumar Jakkaraju <sandeep.jakkaraju@doctily.com>
2018-04-20 21:05:34 +0530

    sckit learn compatible class

--- jakgraph/gclassifier.py
+++ jakgraph/gclassifier.py
@@ -2,24 +2,29 @@ from ruruki.graphs import Graph
 import pandas as pd
 import numpy as np
 import random
+import numpy as np
+from sklearn.base import BaseEstimator, ClassifierMixin
+from sklearn.utils.validation import check_X_y, check_array, check_is_fitted
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import euclidean_distances
 
-class gclassifier:
 
- def __init__(self, bins):
-  self.nbins = bins
+class gclassifier(BaseEstimator, ClassifierMixin):
+
+ def __init__(self, nbins):
+  self.nbins = nbins
   self.graph = Graph()
   print("num of bins="+str(self.nbins))
   print(self.graph)
 
- def fit(self,X_train, y_train):
-  print("Hello")
+ def fit(self,X_train, y_train,sample_weight=None):
   nbins = self.nbins
   graph = self.graph
   rows = X_train.shape[0]
   cols = X_train.shape[-1]
   bins = []
-  self.response_classes = np.array(list(set(y_train)))
-  print(len(self.response_classes))
+  self.classes_ = unique_labels(y_train)
+  print(len(self.classes_))
 
   for kk in range (0,cols):
    bins.append(self.get_buckets(X_train.min(axis=0)[kk],X_train.max(axis=0)[kk],nbins))
@@ -29,7 +34,7 @@ class gclassifier:
   for i in range (0,rows):
     for j in range (0,cols):
         wbin=self.find_bin(bins[j],X_train[i][j])
-        print("wbin"+str(wbin))
+        #print("wbin"+str(wbin))
         v = graph.get_or_create_vertex("data",col=j,cbin=wbin)
         out = graph.get_or_create_vertex("outc",out=y_train[i])
         #print(out)
@@ -45,7 +50,7 @@ class gclassifier:
   graph = self.graph
   y_pred =[]
   for i in range (0,X_test.shape[0]):
-      count= np.zeros(len(self.response_classes))
+      count= np.zeros(len(self.classes_))
       totv = []
       for j in range (0,X_test.shape[-1]):
           wbin=self.find_bin(self.bins[j],X_test[i][j])
@@ -53,18 +58,18 @@ class gclassifier:
               totv.append(v)
       for r in totv:
           mv = r
-          print(mv.as_dict().get('properties'))
+          #print(mv.as_dict().get('properties'))
           for m in mv.get_out_edges():
                   #print(m.as_dict().get('properties'))
                   #print(m.get_out_vertex().as_dict())
                   indx = int(str(m.get_out_vertex().as_dict().get('properties').get("out")))
                   count[indx] = count[indx]+int(str(m.as_dict().get('properties').get("count")))
                   #print("index"+str(indx)+" --"+str(count[indx]))
-      print(count)
+      #print(count)
       for ii in range (0,len(count)):
           if(count[ii] == count.max()):
-                  print(self.response_classes[ii])
-                  y_pred.append(self.response_classes[ii])
+                  #print(self.classes_[ii])
+                  y_pred.append(self.classes_[ii])
                   break
   return y_pred
 
@@ -84,3 +89,12 @@ class gclassifier:
    else:
     first_bin = abins[i]
   return -1
+
+def get_params(self, deep=True):
+    # suppose this estimator has parameters "alpha" and "recursive"
+    return {"nbins": self.nbins}
+
+def set_params(self, **parameters):
+    for parameter, value in parameters.items():
+        setattr(self, parameter, value)
+    return self
